413cba74fce5e257db46870c2c00458e49e56eec
dsmadhav12 <dsmadhav@hotmail.com>
2025-06-05 12:28:54 -0400

    updated name of homepage

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -4,7 +4,7 @@ export default function SignIn({ onSignInClick }) {
   return (
     <div className="SignInOverlay">
       <div className="SignInCard">
-        <h1 className="SignInTitle">Welcome to SwingSense</h1>
+        <h1 className="SignInTitle">Welcome to SwingAnalyzer</h1>
         <p className="SignInSubtitle">Sign in with Google to begin tracking your golf swings.</p>
         <button className="SignInButton" onClick={onSignInClick}>
           Sign in with Google
