156faef5fcf80f38c7111473fd0d2357ef346ec0
dsmadhav12 <dsmadhav@hotmail.com>
2025-06-05 12:24:55 -0400

    created pages folder adn signin page

---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,15 @@
+import './SignIn.css';
+
+export default function SignIn({ onSignInClick }) {
+  return (
+    <div className="SignInOverlay">
+      <div className="SignInCard">
+        <h1 className="SignInTitle">Welcome to SwingSense</h1>
+        <p className="SignInSubtitle">Sign in with Google to begin tracking your golf swings.</p>
+        <button className="SignInButton" onClick={onSignInClick}>
+          Sign in with Google
+        </button>
+      </div>
+    </div>
+  );
+}
